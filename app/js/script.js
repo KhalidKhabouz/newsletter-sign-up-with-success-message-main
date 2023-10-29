8f035cc153ab3a4b6d4a89b0d2f242eaededb262
Khalid Khabouz <khalid.khabouz@gmail.com>
2023-10-29 02:24:24 +0200

    finished javascript for desktop

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,34 @@
-console.log('Hello World!');
+console.log("Hello World!");
+const card = document.getElementById("newsletter-card");
+const input = document.getElementById("email-input");
+const submitBtn = document.getElementById("submit-bnt");
+const modalEl = document.getElementsByClassName("modal-success")[0];
+const modalContainer = document.getElementsByClassName("modal-container")[0];
+const dismissBtn = document.querySelector(".modal-success__dismiss-btn");
+const errorTxt = document.querySelector('.error-txt');
+const submitedEmail = document.getElementById("submited-email")
+var validRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/;
+
+function submit() {
+  if (!input.value.match(validRegex)) {
+    errorTxt.style.display = "block";
+    input.style.color = "var(--tomato)";
+    input.classList.add('invalid-input');
+  } else {
+    modalEl.style.display = "flex";
+    modalContainer.style.display = "block";
+    submitedEmail.innerHTML =  input.value;
+  }
+}
+
+function hideModal() {
+  modalEl.style.display = "none";
+  modalContainer.style.display = "none";
+  input.classList.remove('invalid-input');
+  errorTxt.style.display = "none";
+  input.value = "";
+}
+
+submitBtn.addEventListener("click", submit);
+dismissBtn.addEventListener("click", hideModal);
